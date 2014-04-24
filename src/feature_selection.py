59484fbc7078bf8deeea2478d996f98558b3d9ac
Rafael Vieira <rvieira@dcc.ufrj.br>
2014-04-24 19:21:59 -0300

    Adding univariate feature selection.

---
+++ src/feature_selection.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from datatypes import Dataset
+
+from sklearn.feature_selection import SelectKBest, f_classif
+
+def univariate_feature_selection(n, ds):
+    """
+    Selects 'n' features in the dataset. Returns the Reduced Dataset
+    n (int), ds (Dataset) -> Dataset
+    """
+
+    selector = SelectKBest(f_classif, n)
+    selector.fit(ds.data, ds.target)
+    features = selector.get_support(indices=True)
+    return Dataset(selector.transform(ds.data), ds.target)
