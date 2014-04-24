1a3d6f15a383f7cffd03266f44e7de42782602d9
Rafael Vieira <rvieira@dcc.ufrj.br>
2014-04-24 16:28:27 -0300

    Adding a datatypes module.

---
+++ src/datatypes.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from collections import namedtuple
+
+"""
+    This module defines the datatypes used by the other modules.
+"""
+
+Dataset = namedtuple("Dataset", ["data", "target"] )
+
